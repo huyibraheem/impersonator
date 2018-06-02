2b370c23f8a81ccdc7ff80d82f6373774a6b024e
lmaocow <lolcow@protonmail.com>
2018-06-02 01:45:28 +0000

    working

--- googlesearch.py
+++ googlesearch.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
-from BeautifulSoup import BeautifulSoup 
+from bs4 import BeautifulSoup 
 import re 
-import urllib2 
+from urllib.request import urlopen
 import random 
 def googlesearch(): 
     newtab = 0
     while True:
-        if newtab != 0: 
+        if newtab == 0: 
             driver = webdriver.Firefox() 
             driver.get("https://www.google.com")
             timeout = 20
@@ -15,8 +15,8 @@ def googlesearch():
             input_element = driver.find_element_by_name("q")
             input_element.send_keys(query)
             input_element.submit()
-            browser.close() 
-        elif newtab < 6: # firefox is already open, and we have less than 6 tabs
+            newtab = newtab + 1
+        elif newtab < 6 and newtab != 0: # firefox is already open, and we have less than 6 tabs
             r = random.randint(1,2) # choose between opening new tab or following links 
             if r == 1: 
                 driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + 't')
@@ -30,8 +30,9 @@ def googlesearch():
             elif r == 2:
                 links = []
                 interval = random.randint(20, 100) # sleep interval for clicking 
-                html = urllib2.urlopen(driver.current_url)
+                html = urlopen(driver.current_url)
                 soup = BeautifulSoup(html)
-                for i in soup.findAll('a', attrs=('href': re.compile("^http://"))): 
-                    links.append(i.get('href')
-
+                for i in soup.findAll('a', attrs={'href': re.compile("^http://")}): 
+                    links.append(i.get('href'))
+                goto = random.choice(links)
+                driver.get(goto)
